99d738137c7e2f73a353053b322bcfd3eff72647
vjbisht1122 <bishtvijay437@gmail.com>
2026-02-18 23:52:55 +0530

    make changes in redux slice and created local storage now all my notes are gettig stored in the browser local storage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='flex flex-row gap-4 place-content-evenly '>
        <NavLink to="/">
-        Home 
+        Home page
       </NavLink>
       <NavLink to="/pastes">
         Paste
